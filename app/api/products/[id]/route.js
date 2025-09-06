fcdd167f6253bfce8efb77e3e1ab4e78a299012c
Otibaco <orjifranklin42@gmail.com>
2025-09-06 01:22:50 +0100

    fixing error in production2

--- app/api/products/[id]/route.js
+++ app/api/products/[id]/route.js
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import { connectDB } from "@/lib/connectDB";
-import Product from "@/models/Product";
 import { requireAdmin } from "@/lib/auth-guard";
+import Product from "models/Product";
 
 export async function GET(_req, { params }) {
   await connectDB();
